bc210517035462b6ef7813892df4266699c9ffe2
apike <apike@illumina.com>
2019-07-12 13:28:31 -0700

    Proposed changes needs testing.

--- src/StateMachine.h
+++ src/StateMachine.h
@@ -2,7 +2,9 @@
 
 #include <map>
 
-#define NO_STATE 0
+#define RESERVED_NO_STATE -1
+#define RESERVED_NO_STATE_CHANGE -2
+
 #define RESERVED_TRIGGER_DEFAULT_ENTRY -1
 #define RESERVED_TRIGGER_DEFAULT_EXIT -2
 
@@ -11,7 +13,7 @@ class State
 public:
 	virtual void EntryAction() = 0;
 	virtual void ExitAction() = 0;
-	unsigned int virtual Trigger(int trigger) = 0;
+	int virtual Trigger(int trigger) = 0;
 };
 
 template <class T>
@@ -19,23 +21,22 @@ class StateTemplate : public State
 {
 
 protected:	
-	typedef unsigned int (T::* Guard)(unsigned int);
-	T* _derivedState;
+	typedef unsigned int (T::* Guard)(unsigned int);	
 	std::map<unsigned int, Guard> _triggers;
 
 public:	
 	
-	unsigned int virtual Trigger(int trigger)
+	int virtual Trigger(int trigger)
 	{
 		switch(trigger)
 		{
 		case RESERVED_TRIGGER_DEFAULT_EXIT:
-			return RESERVED_TRIGGER_DEFAULT_EXIT;
+			return RESERVED_NO_STATE;
 		default:
 		{
 			if (_triggers.find(trigger) == _triggers.end())
 			{
-				return NO_STATE; // Not supported transition.
+				return RESERVED_NO_STATE_CHANGE; // Not supported transition.
 			}
 			else
 			{
@@ -46,6 +47,7 @@ public:
 		break;
 		}
 	}
+
 	void AddTriggerGuard(unsigned int trigger, Guard guard)
 	{
 		_triggers.emplace(trigger, guard);
@@ -59,29 +61,51 @@ class OrState : public StateTemplate<T>
 private:
 	std::map<unsigned int, State*> _childStates;
 	int _defaultEntryState = U;
+	int _currentState;
 
 	void ChangeState(unsigned int newState)
 	{
-		if (_currentState != NULL)
+		if (newState == RESERVED_NO_STATE_CHANGE)
 		{
-			_currentState->ExitAction();
+			return;
 		}
 
-		if (_childStates.find(newState) != _childStates.end())
+		if (_currentState != RESERVED_NO_STATE)
 		{
-			_currentState = _childStates[newState];
-			_currentState->EntryAction();
+			_childStates[CurrentState]->ExitAction();
+		}
+
+		if (newState == RESERVED_NO_STATE)
+		{
+			_currentState = RESERVED_NO_STATE;
+		}
+		else if(_childStates.find(newState) != _childStates.end())
+		{
+			_currentState = newState;			
+			_childStates[_currentState]->EntryAction();
 		}
 	}
-
-protected:
-	State* _currentState;
 
 public:
+	
 	void AddState(unsigned int enumValue, State& instance)
 	{
+		// Negative values are reserved for internal private
+		// use so insigned int is by design here...
 		_childStates.emplace(enumValue, &instance);
 	}
+
+	void virtual EntryAction()
+	{
+		Trigger(RESERVED_TRIGGER_DEFAULT_ENTRY);
+	}
+
+	void virtual ExitAction()
+	{
+		Trigger(RESERVED_TRIGGER_DEFAULT_EXIT);
+	}
+
+	int GetCurrentState() { return _currentState; }
 
 	unsigned int virtual Trigger(int trigger)
 	{
@@ -89,23 +113,23 @@ public:
 		{
 		case RESERVED_TRIGGER_DEFAULT_ENTRY:
 		{
-			if (_currentState != NULL)
+			if (_currentState != RESERVED_NO_STATE)
 			{
-				return RESERVED_TRIGGER_DEFAULT_ENTRY;
+				return RESERVED_NO_STATE_CHANGE;
 			}
 			ChangeState(_defaultEntryState);
 		}
 		break;
 		case RESERVED_TRIGGER_DEFAULT_EXIT:
-		{
-			_currentState = NULL;
+		{			
+			ChangeState(RESERVED_NO_STATE);
 		}
 		break;
 		default:
 		{
-			if (_currentState != NULL)
+			if (_currentState != RESERVED_NO_STATE)
 			{
-				unsigned int newState = _currentState->Trigger(trigger);
+				unsigned int newState = _childStates[_currentState]->Trigger(trigger);
 				ChangeState(newState);
 			}
 		}
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include "StateMachine.h"
 
-enum STATES {IDLE = 1};
-enum TRIGGERS { IDLETRIGGER = 1 };
+enum STATES {IDLE = 1, FINAL = 2};
+enum TRIGGERS { IDLETRIGGER = 1, FINALTRIGGER = 2 };
 
 class Idle : public StateTemplate<Idle>
 {
 private:
-	unsigned int TransitionFromIdle(unsigned int);
+	unsigned int TransitionToIdle(unsigned int);
+	unsigned int TransitionToFinal(unsigned int);
 
 public:
 	Idle();
@@ -16,12 +17,39 @@ public:
 
 Idle::Idle()
 {
-	AddTriggerGuard(IDLETRIGGER, &Idle::TransitionFromIdle);
+	AddTriggerGuard(IDLETRIGGER, &Idle::TransitionToIdle);
+	AddTriggerGuard(FINALTRIGGER, &Idle::TransitionToFinal);
 }
 
-unsigned int Idle::TransitionFromIdle(unsigned int)
+unsigned int Idle::TransitionToIdle(unsigned int)
 {
-	return NO_STATE;
+	return IDLE;
+}
+
+unsigned int Idle::TransitionToFinal(unsigned int)
+{
+	return FINAL;
+}
+
+class Final : public StateTemplate<Final>
+{
+private:
+	unsigned int TransitionToIdle(unsigned int);
+
+public:
+	Final();
+	void virtual EntryAction() {};
+	void virtual ExitAction() {};
+};
+
+Final::Final()
+{
+	AddTriggerGuard(IDLETRIGGER, &Final::TransitionToIdle);
+}
+
+unsigned int Final::TransitionToIdle(unsigned int)
+{
+	return IDLE;
 }
 
 class SimpleStateMachine : public OrState<SimpleStateMachine, IDLE>
@@ -36,17 +64,18 @@ public:
 SimpleStateMachine::SimpleStateMachine()
 {
 	Idle* idle = new Idle();
+	Final* final = new Final();
+
 	AddState(IDLE, *idle);
+	AddState(FINAL, *final);
 }
 
 void SimpleStateMachine::EntryAction()
 {
-
 }
 
 void SimpleStateMachine::ExitAction()
 {
-
 }
 
 int main(void)
@@ -57,5 +86,8 @@ int main(void)
 	sm.Trigger(RESERVED_TRIGGER_DEFAULT_ENTRY);
 
 	sm.Trigger(IDLETRIGGER);
+	sm.Trigger(IDLETRIGGER);
+
+	sm.Trigger(FINALTRIGGER);
 	return 0;
 }
